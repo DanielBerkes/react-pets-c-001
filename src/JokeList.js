6a5c1aab25c30e7e5ae2b99a3dd233ce72d809e5
Berkes <daniel.berkes@accenture.com>
2020-10-08 07:49:08 +0200

    Styling The Fetch Button

--- src/JokeList.js
+++ src/JokeList.js
@@ -81,7 +81,7 @@ export default class JokeList extends Component {
             alt=""
           />
           <button className="JokeList-getmore" onClick={this.handleClick}>
-            New Jokes
+            Fetch Jokes
           </button>
         </div>
         <div className="JokeList-jokes">
