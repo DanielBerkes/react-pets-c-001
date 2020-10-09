afccaa2b2d983207f33aeac5fefb713093d36974
Berkes <daniel.berkes@accenture.com>
2020-10-09 20:31:14 +0200

    Intro to the Link Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
 import About from "./About";
 import Dog from "./Dog";
 import Contact from "./Contact";
@@ -9,6 +9,14 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <nav>
+          <Link to="/">About</Link>
+          <Link to="/dog">Dog</Link>
+          <Link to="/contact">Contact</Link>
+          <a href="/dog"> Dog </a>
+          <a href="/contact"> Contact </a>
+          <a href="/"> About </a>
+        </nav>
         <Switch>
           <Route exact path="/" component={About} />
           <Route exact path="/dog" component={Dog} />
